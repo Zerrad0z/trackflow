e5f67f9f6e8b0298e94ca79afa227b3483581bfc
Oussama Zd <zedussama@gmail.com>
2026-05-08 12:36:20 +0100

    SCRUM-37: Add User entity, UserRole enum and UserRepository

---
+++ trackflow-backend/src/main/java/com/trackflow/module/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.trackflow.module.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    String fullName;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    UserRole role;
+
+    @Column(nullable = false)
+    Boolean isActive = true;
+
+    @Column(nullable = false, updatable = false)
+    LocalDateTime createdAt = LocalDateTime.now();
+
+    LocalDateTime updatedAt = LocalDateTime.now();
+}
